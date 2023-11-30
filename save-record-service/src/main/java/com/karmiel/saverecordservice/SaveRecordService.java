6b3461e79ac5d398236350460abbdec0a445fc95
Igor <igorekminko@gmail.com>
2023-11-30 19:59:18 +0200

    Add comment

--- save-record-service/src/main/java/com/karmiel/saverecordservice/SaveRecordService.java
+++ save-record-service/src/main/java/com/karmiel/saverecordservice/SaveRecordService.java
@@ -2,7 +2,7 @@ package com.karmiel.saverecordservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Minko Igor
 @SpringBootApplication
 public class SaveRecordService {
     public static void main(String[] args) {
