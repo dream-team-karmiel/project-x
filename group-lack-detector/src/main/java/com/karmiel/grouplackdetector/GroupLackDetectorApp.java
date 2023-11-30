700ce456e8807e1c446bf9dc8086d44cd2f22b35
Pavel Dubovik <pavel.djub@gmail.com>
2023-11-30 15:51:48 +0200

    Add comment

--- group-lack-detector/src/main/java/com/karmiel/grouplackdetector/GroupLackDetectorApp.java
+++ group-lack-detector/src/main/java/com/karmiel/grouplackdetector/GroupLackDetectorApp.java
@@ -3,6 +3,7 @@ package com.karmiel.grouplackdetector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Pavel Dubovik
 @SpringBootApplication
 public class GroupLackDetectorApp {
     public static void main(String[] args) {
