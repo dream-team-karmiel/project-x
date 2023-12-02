7874cad95828df9ea6a6f26148e82f0576750030
Alexey Morozov <morozof@gmail.com>
2023-12-02 13:00:52 +0200

    Add maven properties to pom file. Add DTO, application and test file. Test communication with fake-kafka passed.

--- group-lack-detector/src/main/java/com/karmiel/grouplackdetector/GroupLackDetectorApp.java
+++ group-lack-detector/src/main/java/com/karmiel/grouplackdetector/GroupLackDetectorApp.java
@@ -1,13 +1,41 @@
 package com.karmiel.grouplackdetector;
 
+import com.karmiel.grouplackdetector.dto.ContainerData;
+import com.karmiel.grouplackdetector.dto.FullData;
+import com.karmiel.grouplackdetector.dto.OrderData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class GroupLackDetectorApp {
-
+    @Autowired
+    StreamBridge bridge;
+    double threshold = 50;
+    double volume = 100;
 
     public static void main(String[] args) {
         SpringApplication.run(GroupLackDetectorApp.class);
+    }
+
+    @Bean
+    Consumer<ContainerData> recieveContainerData() {
+
+        return data -> {
+
+            double fullness = volume / 100 * data.quantity();
+            if (fullness < threshold) {
+                System.out.println("Volume < 50%");
+                bridge.send("order-out-0",
+                        new OrderData(data.spotCoordinates(), "TestProductName", volume - data.quantity()));
+            } else if (fullness >= threshold) {
+                System.out.println("Volume > 50%");
+                bridge.send("full-out-0", new FullData(data.spotCoordinates()));
+            }
+        };
     }
 }
--- group-lack-detector/src/test/java/com/karmiel/grouplackdetector/GroupLackDetectorTest.java
+++ group-lack-detector/src/test/java/com/karmiel/grouplackdetector/GroupLackDetectorTest.java
@@ -1,4 +1,52 @@
 package com.karmiel.grouplackdetector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.karmiel.grouplackdetector.dto.ContainerData;
+import com.karmiel.grouplackdetector.dto.FullData;
+import com.karmiel.grouplackdetector.dto.OrderData;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
 public class GroupLackDetectorTest {
+    @Autowired
+    InputDestination producer;
+    @Autowired
+    OutputDestination consumer;
+    ObjectMapper mapper = new ObjectMapper();
+    ContainerData bigContainerData = new ContainerData("001", 70.0);
+    ContainerData smallContainerData = new ContainerData("001", 40.0);
+    FullData fullDataExpected = new FullData("001");
+    OrderData orderDataExpected = new OrderData("001", "TestProductName", 60.0);
+
+    @Test
+    void lackDetectorTest() throws IOException {
+
+
+        producer.send(new GenericMessage<ContainerData>(bigContainerData), "container-in-0");
+        Message<byte[]> messageBig = consumer.receive(100, "full-out-0" );
+        assertNotNull(messageBig);
+        assertEquals(fullDataExpected, mapper.readValue(messageBig.getPayload(), FullData.class));
+
+        producer.send(new GenericMessage<ContainerData>(smallContainerData), "container-in-0");
+        Message<byte[]> messageSmall = consumer.receive(100, "order-out-0" );
+        assertNotNull(messageSmall);
+        assertEquals(orderDataExpected, mapper.readValue(messageSmall.getPayload(), OrderData.class));
+
+
+    }
+
 }
