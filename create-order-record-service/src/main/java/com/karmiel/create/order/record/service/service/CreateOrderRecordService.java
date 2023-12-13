9728501680d7b52596e14ed8bc44edae4136520f
m.denisov <mihaildienisov@gmail.com>
2023-12-13 04:06:25 +0200

    add CreateOrderRecordService interface to service package

---
+++ create-order-record-service/src/main/java/com/karmiel/create/order/record/service/service/CreateOrderRecordService.java
@@ -0,0 +1,7 @@
+package com.karmiel.create.order.record.service.service;
+
+import java.util.function.Function;
+
+public interface CreateOrderRecordService {
+    Function<String, String> createOrderRecord();
+}
