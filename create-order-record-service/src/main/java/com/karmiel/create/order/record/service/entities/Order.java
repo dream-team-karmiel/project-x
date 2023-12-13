5bd179467cf0da67fec1a0299203e99a95e62ac2
m.denisov <mihaildienisov@gmail.com>
2023-12-13 05:06:00 +0200

    add Orders and OrderStatus classes to entities package. May be it's not necessary

---
+++ create-order-record-service/src/main/java/com/karmiel/create/order/record/service/entities/Order.java
@@ -0,0 +1,27 @@
+package com.karmiel.create.order.record.service.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash
+public class Order {
+    @Id
+    private String id;
+    private String spotCoordinates;
+    private LocalDateTime creationDate;
+    private LocalDateTime closeDate;
+    private String productName;
+    private Double requiredQuantity;
+    private OrderStatus orderStatus;
+
+}
---
+++ create-order-record-service/src/main/java/com/karmiel/create/order/record/service/entities/OrderStatus.java
@@ -0,0 +1,4 @@
+package com.karmiel.create.order.record.service.entities;
+
+public enum OrderStatus {
+}
