058ce6f5299dbc45f06884f72826b0aa7d2ea63b
m.denisov <mihaildienisov@gmail.com>
2023-12-13 07:39:29 +0200

    add existBy method to OrdersRecordsRepo

--- create-order-record-service/src/main/java/com/karmiel/create/order/record/service/repo/OrdersRecordsRepo.java
+++ create-order-record-service/src/main/java/com/karmiel/create/order/record/service/repo/OrdersRecordsRepo.java
@@ -4,4 +4,5 @@ import com.karmiel.create.order.record.service.entities.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface OrdersRecordsRepo extends MongoRepository<Order, String> {
+    Boolean existsBySpotCoordinatesAndOrderStatusIn(String spotCoordinates, String[] statuses);
 }
