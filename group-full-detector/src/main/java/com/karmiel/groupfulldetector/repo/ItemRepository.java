0e0acbe479da413c8b8e033defacab6ff7f53da0
Mark Klotsman <mark.klocman@gmail.com>
2023-12-14 16:07:53 +0200

    deleted unused query and updated type of method findOrderBySpotCoordinate

--- group-full-detector/src/main/java/com/karmiel/groupfulldetector/repo/ItemRepository.java
+++ group-full-detector/src/main/java/com/karmiel/groupfulldetector/repo/ItemRepository.java
@@ -1,20 +1,13 @@
 package com.karmiel.groupfulldetector.repo;
 
-import com.karmiel.groupfulldetector.dto.OrderItem;
+import com.karmiel.groupfulldetector.enities.OrderData;
+import com.karmiel.groupfulldetector.utils.OrderStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public interface ItemRepository extends MongoRepository<OrderItem, Integer> {
-    @Query(value="{spotCoordinates: '?0'}", fields = "{'id' :  1, 'orderStatus': 1}")
-    List<OrderItem> findAllOrdersBySpotCoordinates (String spotCoordinates);
-
-    @Query("{id: '?0'}")
-    OrderItem findItemByOrderId (long id);
-
-    public long count();
-
-
-
+public interface ItemRepository extends MongoRepository<OrderData, String> {
+    @Query(value="{spotCoordinates: '?0', orderStatus: OrderStatus.CONFIRMED}", fields = "{'id' :  1, 'spotCoordinates' : 1, 'orderStatus': 1}")
+    Optional<OrderData> findOrderBySpotCoordinatesAndStatus (String spotCoordinates);
 }
