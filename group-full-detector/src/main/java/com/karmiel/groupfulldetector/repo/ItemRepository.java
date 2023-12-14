ce16293feb0b50a828a7081fa19446a48a05c4cc
Mark Klotsman <mark.klocman@gmail.com>
2023-12-14 16:09:32 +0200

    deleted unused input

--- group-full-detector/src/main/java/com/karmiel/groupfulldetector/repo/ItemRepository.java
+++ group-full-detector/src/main/java/com/karmiel/groupfulldetector/repo/ItemRepository.java
@@ -1,13 +1,12 @@
 package com.karmiel.groupfulldetector.repo;
 
 import com.karmiel.groupfulldetector.enities.OrderData;
-import com.karmiel.groupfulldetector.utils.OrderStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface ItemRepository extends MongoRepository<OrderData, String> {
-    @Query(value="{spotCoordinates: '?0', orderStatus: OrderStatus.CONFIRMED}", fields = "{'id' :  1, 'spotCoordinates' : 1, 'orderStatus': 1}")
+    @Query(value="{spotCoordinates: '?0', orderStatus:'CONFIRMED'}", fields = "{'id' :  1, 'spotCoordinates' : 1, 'orderStatus': 1}")
     Optional<OrderData> findOrderBySpotCoordinatesAndStatus (String spotCoordinates);
 }
