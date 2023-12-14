dbe3b5ce25f23303fb351d6f4db9889397c6fbc4
Mark Klotsman <mark.klocman@gmail.com>
2023-12-14 16:12:25 +0200

    added tests for microservice

---
+++ group-full-detector/src/test/java/com/karmiel/groupfulldetector/GroupFullDetectorTest.java
@@ -0,0 +1,88 @@
+package com.karmiel.groupfulldetector;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.karmiel.groupfulldetector.dto.FullData;
+import com.karmiel.groupfulldetector.dto.Order;
+import com.karmiel.groupfulldetector.enities.OrderData;
+import com.karmiel.groupfulldetector.repo.ItemRepository;
+import com.karmiel.groupfulldetector.utils.OrderStatus;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.cloud.stream.binder.test.*;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
+public class GroupFullDetectorTest {
+
+    @Autowired //(required = false)
+//    @Resource
+    InputDestination producer;
+    @Autowired
+//    @Resource
+    OutputDestination consumer;
+    @MockBean
+    ItemRepository repository;
+
+   //@Value("${spring.cloud.stream.bindings.full-out-0.destination:check-open-order}")
+    String producerBindingName = "receiveFullData-in-0";
+    //@Value("${spring.cloud.stream.bindings.full-in-0.destination:close-order}")
+   String consumerBindingName = "close-order";
+
+    @Test
+    @DirtiesContext
+    void sendRequestToCloseOrder() throws Exception{
+        String containerId = "A11";
+        FullData fullData = new FullData(containerId);
+
+        OrderData order = new OrderData();
+        order.setId("348");
+        order.setSpotCoordinates(containerId);
+        order.setOrderStatus(OrderStatus.CONFIRMED);
+
+        when(repository.findOrderBySpotCoordinatesAndStatus(containerId)).thenReturn(Optional.of(order));
+
+        producer.send(new GenericMessage<>(fullData), producerBindingName);
+
+        Message<byte[]> message = consumer.receive(100, consumerBindingName);
+
+        assertNotNull(message);
+        ObjectMapper mapper = new ObjectMapper();
+        assertEquals(order.getId(), mapper.readValue(message.getPayload(), Order.class).orderId());
+    }
+
+    @Test
+    @DirtiesContext
+    void notSendRequestToCloseOrder() throws Exception{
+        String id = "A11";
+        FullData fullData = new FullData(id);
+        OrderData order = new OrderData();
+        order.setId("349");
+        order.setSpotCoordinates(id);
+        order.setOrderStatus(OrderStatus.DONE);
+
+        when(repository.findOrderBySpotCoordinatesAndStatus(id)).thenReturn(Optional.empty());
+
+        producer.send(new GenericMessage<>(fullData), producerBindingName);
+
+        Message<byte[]> message = consumer.receive(100, consumerBindingName);
+        assertNull(message);
+    }
+}
