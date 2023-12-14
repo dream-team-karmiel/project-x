c2f2fea0a93aae9be91eb7611bef3306a015aa26
Mark Klotsman <mark.klocman@gmail.com>
2023-12-14 16:09:46 +0200

    Changes by paveldubovik

--- group-full-detector/src/main/java/com/karmiel/groupfulldetector/repo/ItemRepository.java
+++ group-full-detector/src/main/java/com/karmiel/groupfulldetector/repo/ItemRepository.java
@@ -3,8 +3,7 @@ package com.karmiel.groupfulldetector.repo;
 import com.karmiel.groupfulldetector.enities.OrderData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-
-import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends MongoRepository<OrderData, String> {
     @Query(value="{spotCoordinates: '?0', orderStatus:'CONFIRMED'}", fields = "{'id' :  1, 'spotCoordinates' : 1, 'orderStatus': 1}")
