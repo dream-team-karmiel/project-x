0995a34c0ad270a49895b418b62beaed3e0dd69c
yacax <yacax@yandex.ru>
2023-12-17 12:50:42 +0200

    Fix bugs based on review comments

--- data-reducer/src/main/java/com/karmiel/reducer/controller/SensorController.java
+++ data-reducer/src/main/java/com/karmiel/reducer/controller/SensorController.java
@@ -1,8 +1,9 @@
 package com.karmiel.reducer.controller;
 
-
 import com.karmiel.reducer.Sensor;
 import com.karmiel.reducer.service.SensorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 
@@ -10,7 +11,9 @@ import java.util.function.Consumer;
 
 @Controller
 public class SensorController {
+    private static final Logger logger = LoggerFactory.getLogger(SensorController.class);
     private final SensorService service;
+
 
     public SensorController(SensorService service) {
         this.service = service;
@@ -18,6 +21,9 @@ public class SensorController {
 
     @Bean
     public Consumer<Sensor> sensorConsumer() {
-        return service::saveSensor;
+        return sensor -> {
+            logger.trace("Sensor processing with coordinates: {}, and value: {}", sensor.spotCoordinates(), sensor.quantity());
+            service.saveSensor(sensor);
+        };
     }
 }
--- data-reducer/src/main/java/com/karmiel/reducer/service/SensorServiceImp.java
+++ data-reducer/src/main/java/com/karmiel/reducer/service/SensorServiceImp.java
@@ -3,15 +3,21 @@ package com.karmiel.reducer.service;
 import com.karmiel.reducer.Sensor;
 import com.karmiel.reducer.repository.SensorRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class SensorServiceImp implements SensorService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SensorServiceImp.class);
+
 
     private final StreamBridge streamBridge;
     private final SensorRepository repository;
@@ -22,13 +28,18 @@ public class SensorServiceImp implements SensorService {
     @Override
     public void saveSensor(Sensor sensor) {
         Optional<Sensor> optionalSensor = repository.findById(sensor.spotCoordinates());
-        if (optionalSensor.isEmpty() || notEqualQuantity(optionalSensor.get(), sensor)) {
+        if (optionalSensor.isEmpty() || !isEqualQuantity(optionalSensor.get(), sensor)) {
+            logger.info("Saving the new sensor state with coordinates: {}", sensor.spotCoordinates());
             repository.save(sensor);
+            logger.info("Sending a message about the sensor to the topic '{}'", newTopic);
             streamBridge.send(newTopic, sensor);
+        } else {
+            logger.info("The sensor status has not changed, saving and sending a message is not required.");
         }
+
     }
 
-    private boolean notEqualQuantity(Sensor existingSensor, Sensor newSensor) {
-        return !newSensor.quantity().equals(existingSensor.quantity());
+    private boolean isEqualQuantity(Sensor existingSensor, Sensor newSensor) {
+        return Objects.equals(newSensor.quantity(), existingSensor.quantity());
     }
 }
