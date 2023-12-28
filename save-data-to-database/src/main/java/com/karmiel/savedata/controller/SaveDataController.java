5510de179e06f4db6adc5bf09f777106bd65a2b8
Pavel Dubovik <pavel.djub@gmail.com>
2023-12-28 22:36:32 +0200

    fix: add injection

--- save-data-to-database/src/main/java/com/karmiel/savedata/controller/SaveDataController.java
+++ save-data-to-database/src/main/java/com/karmiel/savedata/controller/SaveDataController.java
@@ -2,6 +2,7 @@ package com.karmiel.savedata.controller;
 
 import com.karmiel.savedata.dto.QuantityDto;
 import com.karmiel.savedata.service.SavaDataService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
@@ -10,11 +11,12 @@ import java.util.function.Consumer;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class SaveDataController {
-    SavaDataService service;
+    private final SavaDataService service;
 
     @Bean
-    Consumer<QuantityDto> receiveNewSensor() {
+    Consumer<QuantityDto> receiveNewData() {
         return data ->  {
             log.info("Data spotCoordinates {}, quantity {} received", data.spotCoordinates(), data.quantity());
             service.saveData(data);
--- save-data-to-database/src/main/java/com/karmiel/savedata/service/SavaDataServiceImpl.java
+++ save-data-to-database/src/main/java/com/karmiel/savedata/service/SavaDataServiceImpl.java
@@ -4,14 +4,16 @@ import com.karmiel.savedata.dto.QuantityDto;
 import com.karmiel.savedata.dto.QuantityMapper;
 import com.karmiel.savedata.model.Quantity;
 import com.karmiel.savedata.repository.QuantityRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class SavaDataServiceImpl implements SavaDataService {
-    QuantityMapper mapper;
-    QuantityRepository repository;
+    private final QuantityMapper mapper;
+    private final QuantityRepository repository;
 
     @Override
     public void saveData(QuantityDto data) {
