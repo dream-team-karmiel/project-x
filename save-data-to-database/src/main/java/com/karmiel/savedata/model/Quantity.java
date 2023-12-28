39b29e4180dc387b408677adc4c35b2283df3c1f
Pavel Dubovik <pavel.djub@gmail.com>
2023-12-29 01:14:46 +0200

    feature: add save-data to docker-compose

--- save-data-to-database/src/main/java/com/karmiel/savedata/model/Quantity.java
+++ save-data-to-database/src/main/java/com/karmiel/savedata/model/Quantity.java
@@ -1,8 +1,6 @@
 package com.karmiel.savedata.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name="quantities")
 public class Quantity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String containerId;
     private LocalDateTime sensorDate;
