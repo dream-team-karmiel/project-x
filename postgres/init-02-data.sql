d6587abaf5f891a028145e1e02e120fda9c4cae0
Pavel Dubovik <pavel.djub@gmail.com>
2023-12-29 12:13:14 +0200

    fix: add sql scripts

---
+++ postgres/init-02-data.sql
@@ -0,0 +1,16 @@
+INSERT INTO MEASURES (measure_name) VALUES ('liter');
+
+INSERT INTO PRODUCTS (product_name, measure_id, capacity) VALUES ('Water', 1, 100);
+
+INSERT INTO CONTAINERS (spot_coordinates, product_id)
+VALUES
+    ('A01', 1),
+    ('A02', 1),
+    ('A03', 1),
+    ('A04', 1),
+    ('A05', 1),
+    ('A06', 1),
+    ('A07', 1),
+    ('A08', 1),
+    ('A09', 1),
+    ('A10', 1);
